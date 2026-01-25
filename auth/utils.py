ad176af9b5df6fa0d1579dcc7fa05671192e4f9d
oKhodus <exalchel@gmail.com>
2026-01-26 01:14:09 +0200

    Add generator of JWT

--- auth/utils.py
+++ auth/utils.py
@@ -1,6 +1,22 @@
 import bcrypt
+import jwt as pyjwt
+from datetime import datetime, timedelta
+from config import ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, MAX_BCRYPT_BYTES
+from dotenv import load_dotenv
+import os
 
-MAX_BCRYPT_BYTES = 72
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    token = pyjwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
 
 def hash_password(password: str) -> str:
     password_bytes = password.encode("utf-8")[:MAX_BCRYPT_BYTES]
